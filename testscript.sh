4225c2148769aee66eee2d99f4bda5a971de9a73
rdsaini <45668088+rdsaini@users.noreply.github.com>
2020-03-31 19:54:42 -0400

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "This script is from github"
-NUMBER=$($NUMBERONE + $NUMBERTWO)
+NUMBER = $(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
 echo "This is complete"
 echo "This is fully complete"
