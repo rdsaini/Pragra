b523300d53de34a25e02815c273ec49c57092706
rdsaini <45668088+rdsaini@users.noreply.github.com>
2020-03-31 19:29:08 -0400

    Create testscript.sh

---
+++ testscript.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "This script is from github"
+NUMBER=$(($NUMBERONE + $NUMBERTWO))
+echo "$NUMBER"
+echo "This is complete"
+echo "This is fully complete"
+echo "This was run automatically"
