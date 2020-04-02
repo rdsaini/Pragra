ee0c5b499c3ba59f19d47a5cb05cc480ff9c1233
rdsaini <45668088+rdsaini@users.noreply.github.com>
2020-04-02 15:37:25 -0400

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-#echo "This script is from github"
+echo "This script is from github"
 NUM=$((FIRSTNUM + SECONDNUM))
 #NUMBER = $(($NUMBERONE + $NUMBERTWO))
 echo "$NUM"
+echo "$FIRSTNUM"
+echo "SECONDNUM"
 echo "This is complete"
 echo "This is fully complete 2"
 echo "This was run automatically"
