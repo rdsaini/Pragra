fd656c9ae9bf33c07f41a276bd620a4897ec47ba
raman Saini <rdsaini@linuxacademy.com>
2020-04-03 00:54:50 +0000

    script updated

--- testscript.sh
+++ testscript.sh
@@ -2,9 +2,10 @@
 echo "This script is from github"
 NUM=$((FIRSTNUM + SECONDNUM))
 #NUMBER = $(($NUMBERONE + $NUMBERTWO))
-echo "$NUM"
-echo "$FIRSTNUM"
-echo "$SECONDNUM"
+#echo "$NUM"
+echo "First number is : $FIRSTNUM"
+echo "Second number is : $SECONDNUM"
 echo "This is complete"
+echo "$NUM"
 echo "This is fully complete 5"
 echo "This was run automatically"
